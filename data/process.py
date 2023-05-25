480650bae3794875ae7c7249571c9ca998e1dcd9
xiaoa0 <xiaoa0@users.noreply.github.com>
2023-05-25 17:18:56 +0000

    deploy: 1b798af8cdaa01cf1839c42db71b5f5e6662fc69

--- data/process.py
+++ data/process.py
@@ -1,26 +1,11 @@
 import pandas as pd
-import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-import re
-import string
+data = pd.read_csv('Fake.csv',index_col=0)
+data.head()
 
-df_fake = pd.read_csv("/data/Fake.csv")
-df_true = pd.read_csv("/data/True.csv")
+data.shape
 
-df_fake.head()
-
-
-df_true.head(5)
-
-df_fake["class"] = 0
-df_true["class"] = 1
-
-df_fake.shape, df_true.shape
-
-df_fake_manual_testing["class"] = 0
-df_true_manual_testing["class"] = 1
-
-df_true_manual_testing.head(10)
-df_false_manual_testing.head(10)
+data = data.drop(["title", "subject","date"], axis = 1)
+data.isnull().sum()
