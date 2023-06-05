7641e6c340030add2893777d807c447dfaf2c7a1
xiaoa0 <xiaoallie@gmail.com>
2023-06-05 10:09:08 -0700

    js!!

--- script.js
+++ script.js
@@ -1,58 +1,41 @@
-// Create an input element to allow the user to select a file
-var input = document.createElement('input');
-input.type = 'file';
+document.addEventListener('DOMContentLoaded', function() {
+    var csvFileInput = document.getElementById('csvFileInput');
+    var csvTable = document.getElementById('csvTable');
 
-// Create a table element to display the CSV data
-var table = document.createElement('table');
-document.body.appendChild(table);
+    csvFileInput.addEventListener('change', function(e) {
+        var file = e.target.files[0];
+        var reader = new FileReader();
 
-// Add an event listener to handle file selection
-input.addEventListener('change', function(e) {
-    var file = e.target.files[0];
+        reader.onload = function(e) {
+            var csvContent = e.target.result;
+            var rows = csvContent.split('\n');
+            var csvData = [];
 
-    // Create a FileReader object
-    var reader = new FileReader();
+            for (var i = 0; i < rows.length; i++) {
+                var cells = rows[i].split(',');
+                csvData.push(cells);
+            }
 
-    // Define the onload event handler to execute when the file is read
-    reader.onload = function(e) {
-        var csvContent = e.target.result;
-        
-        // Parse the CSV content
-        var rows = csvContent.split('\n');
-        var csvData = [];
-        for (var i = 0; i < rows.length; i++) {
-            var cells = rows[i].split(',');
-            csvData.push(cells);
+            displayCSVData(csvData);
+        };
+
+        reader.readAsText(file);
+    });
+
+    function displayCSVData(data) {
+        csvTable.innerHTML = '';
+
+        for (var i = 0; i < data.length; i++) {
+            var row = document.createElement('tr');
+
+            for (var j = 0; j < data[i].length; j++) {
+                var cell = document.createElement('td');
+                cell.textContent = data[i][j];
+                row.appendChild(cell);
+            }
+
+            csvTable.appendChild(row);
         }
-
-        // Display the CSV data
-        displayCSVData(csvData);
-    };
-
-    // Read the file as text
-    reader.readAsText(file);
-});
-
-// Function to display the CSV data in an HTML table
-function displayCSVData(data) {
-    // Clear the existing table content
-    table.innerHTML = '';
-
-    // Create table rows and cells
-    for (var i = 0; i < data.length; i++) {
-        var row = document.createElement('tr');
-
-        for (var j = 0; j < data[i].length; j++) {
-            var cell = document.createElement('td');
-            cell.textContent = data[i][j];
-            row.appendChild(cell);
-        }
-
-        table.appendChild(row);
     }
-}
-
-// Trigger the file selection dialog when the page is loaded
-window.addEventListener('DOMContentLoaded', function() {
-    document.body.appendChild(input);
 });
+
